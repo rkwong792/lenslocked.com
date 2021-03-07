380726eed50af86547c96667bb1617fb981b7ec2
Richard Kwong <rkwong792@gmail.com>
2021-03-07 13:23:17 -0500

    Created our first web server!

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+//Takes in a ResponseWriter - this is where we write our response to the user
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	//built in router where we want to map the request. No matter what path they give us, send to the handlerFunc
+	http.HandleFunc("/", handlerFunc)
+
+	//localhost:3000, port 3000
+	//starts up the server
+	http.ListenAndServe(":3000", nil) //when the second parameter, the handler, is nil, use the DefaultServeMux
 }
